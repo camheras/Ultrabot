dfbb0b83291310aaddaaaae2d71c4923d7706afd
cmaheras <camheras@gmail.com>
2021-04-17 20:58:58 +0200

    add binanceTS.py

--- Indicateur.py
+++ Indicateur.py
@@ -1,11 +1,27 @@
+from binance.client import Client
+import pandas as pd
+import binancekey
+from binanceTS import BinanceTS
+
+
 class Indicateur:
     values = {}
+    ts = None
 
     def __init__(self, values):
+        client = Client(binancekey.KEY, binancekey.SECRET)
+        klines = client.get_historical_klines("NANOBTC", Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC")
+
+        df = pd.DataFrame.from_dict(klines)
+        df = df.rename(
+            columns={0: "open_time", 1: "open", 2: "high", 3: "low", 4: "close", 5: "ignore", 6: "close_time", 7: "ignore", 8: "nb_bisic", 9: "ignore", 10: "ignore",
+                     11: "ignore", })
+        df = df.set_index("open_time")
+        self.ts = BinanceTS(df)
         self.values = values
 
     def __getRSI(self, crypto):
-        return 0
+        self.ts.bollinger()
 
     def __getBallinger(self, crypto):
         return 0
--- binanceTS.py
+++ binanceTS.py
@@ -9,15 +9,8 @@ import pandas as pd
 class BinanceTS:
     df = None
 
-    def __init__(self):
-        client = Client(binancekey.KEY, binancekey.SECRET)
-        klines = client.get_historical_klines("NANOBTC", Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC")
-
-        self.df = pd.DataFrame.from_dict(klines)
-        self.df = self.df.rename(
-            columns={0: "open_time", 1: "open", 2: "high", 3: "low", 4: "close", 5: "ignore", 6: "close_time", 7: "ignore", 8: "nb_bisic", 9: "ignore", 10: "ignore",
-                     11: "ignore", })
-        self.df = self.df.set_index("open_time")
+    def __init__(self, df):
+        self.df = df
 
     def bollinger(self, df):
         df['MA20'] = df['close'].rolling(window=20).mean()
@@ -27,9 +20,9 @@ class BinanceTS:
         df['Lower'] = df['MA20'] - (df['20dSTD'] * 2)
         return df
 
-    def RCI(self,df):
+    def RSI(self, df):
         pass
-    
+
     # bollinger(df)[['close', 'MA20', 'Upper', 'Lower']].plot(figsize=(10, 4))
     # plt.grid(True)
     # plt.title(' Bollinger Bands')
